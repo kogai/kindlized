f6ada7d8df96bd823607868fc533176e91b8b500
shinichi-kogai <kogai0121@gmail.com>
2015-03-07 12:00:49 +0900

    再帰関数にコールバックを渡す

--- shelf/index.js
+++ shelf/index.js
@@ -1,6 +1,5 @@
 var regularInterval = require('./lib/regularInterval');
-
-// - 60s間隔で実行される
-// - 規定の変数回実行される
-
-regularInterval( 10, 300, {} );
+var callBack = function( countExec, searchExpression ){
+  console.log( 'executed', countExec, searchExpression );
+};
+regularInterval( 10, 300, {}, callBack );
--- shelf/lib/regularInterval.js
+++ shelf/lib/regularInterval.js
@@ -1,16 +1,21 @@
 // 実行前に現在の実行回数を定義
 var countExec = 0;
 
-var regularInterval = function( times, interval,  searchExpression ){
+var regularInterval = function( times, interval,  searchExpression, callBack ){
   setTimeout( function(){
 
     // 実行の実体
-    console.log( countExec );
+    try{
+      callBack( countExec, searchExpression );
+    }
+    catch( err ){
+      throw err;
+    }
 
     countExec++;
     if( countExec <  times ){
       // times回実行されるまで再帰実行
-      regularInterval( times, interval, searchExpression );
+      regularInterval( times, interval, searchExpression, callBack );
     }else{
       // times回実行されたら終了
       console.log( 'complete.');
