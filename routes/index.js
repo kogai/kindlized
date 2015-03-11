046e234a34d064c5adbb5fdcb787812c0be87a2e
shinichi-kogai <kogai0121@gmail.com>
2015-03-11 18:55:37 +0900

    書籍リストを表示

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,44 @@
-var Q 			= require( 'q' );
-var express 	= require('express');
-var router 		= express.Router();
-var modelAuthor = require( '../author/lib/modelAuthor' );
+var Q 				= require( 'q' );
+var express 		= require('express');
+var router 			= express.Router();
+var modelAuthor 	= require( '../author/lib/modelAuthor' );
+var modelBookList 	= require( '../shelf/lib/modelBookList' );
 
-var fetchAuthorList = function(){
+var fetchAuthorList = function( authorsAndBooks ){
 	var d = Q.defer();
 	var authorList 	= modelAuthor.find( {}, function( err, authors ){
-		d.resolve( authors );
+		authorsAndBooks.authors = authors;
+		d.resolve( authorsAndBooks );
 	});
 	return d.promise;
 };
 
-var renderRouter = function( authors ){
+var fetchBookList = function( authorsAndBooks ){
 	var d = Q.defer();
-	/* GET home page. */
+	var bookList 	= modelBookList.find( {}, function( err, books ){
+		authorsAndBooks.books = books;
+		d.resolve( authorsAndBooks );
+	});
+	return d.promise;
+};
+
+var renderRouter = function( authorsAndBooks ){
+	var d = Q.defer();
+
 	router.get('/', function(req, res) {
 		res.render('index', {
 			title : 'home',
-			authors: authors
+			authors: authorsAndBooks.authors,
+			books: authorsAndBooks.books
 		});
 		d.resolve();
 	});
 	return d.promise;
 }
 
-Q.when()
+Q.when( {} )
 .then( fetchAuthorList )
+.then( fetchBookList )
 .then( renderRouter )
 .done( function(){
 	console.log( 'router complete.' );
