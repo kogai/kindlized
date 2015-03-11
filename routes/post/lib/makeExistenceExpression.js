00e88170719b22f62a0bc25d5cfc12ad915b82af
shinichi-kogai <kogai0121@gmail.com>
2015-03-12 01:23:55 +0900

    [WIP] ルーターを整理

--- routes/post/lib/makeExistenceExpression.js
+++ routes/post/lib/makeExistenceExpression.js
@@ -2,6 +2,6 @@ module.exports = function ( newBook ){
 	// 検索条件オブジェクトのコンストラクター
 	this.SearchIndex 	= 'Books';
     this.Title         	= newBook
-	this.ResponseGroup  = 'Small';
+	this.ResponseGroup  = 'Small, ItemAttributes';
 	this.BrowseNode 	= 465392;
 };
