6d24198af1ff37e632a4589f6da0a4ad822c9835
shinichi-kogai <kogai0121@gmail.com>
2015-03-11 17:07:26 +0900

    authoriASIN調査時に最終調査時を保存

--- librarian/lib/inspectBookList/siftBookList.js
+++ librarian/lib/inspectBookList/siftBookList.js
@@ -7,10 +7,11 @@ module.exports = function( bookList ){
   callBack = function( bookList ){
     var siftedBookList = [];
     for (var i = 0; i < bookList.length; i++) {
+        
       var lastModifyTime  = bookList[i]._id.getTimestamp();
       var todayDate       = moment();
       var diffDay         = todayDate.diff( lastModifyTime, 'days' );
-      
+
       var hasAuthorityASIN = (function( AuthorityASIN ){
           var authorityASINState = false;
           if( AuthorityASIN.length > 0 ) authorityASINState = true;
@@ -21,7 +22,7 @@ module.exports = function( bookList ){
         siftedBookList.push( bookList[i] );
       }
     }
-    console.log( siftedBookList.length );
+    console.log( 'siftedBookList has', siftedBookList.length, 'books.' );
     d.resolve( siftedBookList );
   };
   callBack( bookList );
