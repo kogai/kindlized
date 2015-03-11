2736d1917a6678fc7d0d9bec013e1c6375bd7a75
shinichi-kogai <kogai0121@gmail.com>
2015-03-11 11:33:32 +0900

    [WIP] タブ間隔調整

--- librarian/lib/fetchParentASIN/inspectASIN.js
+++ librarian/lib/fetchParentASIN/inspectASIN.js
@@ -1,24 +1,24 @@
-var opHelper 						   = require( 'apac' ).OperationHelper;
-var makeOpConfig 				   = require( '../../../common/makeOpConfig' );
-var makeInspectExpression  = require( './makeInspectExpression' );
-var modelBookList 				 = require( '../../../shelf/lib/modelBookList' );
-var constant							 = require('../../../common/constant');
+var opHelper				= require( 'apac' ).OperationHelper;
+var makeOpConfig 			= require( '../../../common/makeOpConfig' );
+var makeInspectExpression	= require( './makeInspectExpression' );
+var modelBookList 			= require( '../../../shelf/lib/modelBookList' );
+var constant				= require('../../../common/constant');
 
 module.exports = function( data ){
 	var retryInterval 		= 0;
-	var countExec					= data.countExec;
-	var bookList 					= data.bookList;
-	var book 							= bookList[ countExec ];
+	var countExec			= data.countExec;
+	var bookList 			= data.bookList;
+	var book 				= bookList[ countExec ];
 	var regularInterval		= data.regularInterval;
 
-	var opConfig 					= new makeOpConfig();
+	var opConfig 			= new makeOpConfig();
 	var opInspectBook 		= new opHelper( opConfig );
-	var inspectExpression = new makeInspectExpression( book.ASIN[0] );
+	var inspectExpression 	= new makeInspectExpression( book.ASIN[0] );
 
   opInspectBook.execute( 'ItemLookup', inspectExpression,  function( err, res ){
 		if( err ) console.log( 'inspectBookのレスポンスエラー ', err, res.ItemSearchErrorResponse.Error );
 		try{
-      var AuthorityASIN = res.ItemLookupResponse.Items[0].Item[0].RelatedItems[0].RelatedItem[0].Item[0].ASIN;
+      		var AuthorityASIN = res.ItemLookupResponse.Items[0].Item[0].RelatedItems[0].RelatedItem[0].Item[0].ASIN;
 			modelBookList.findOneAndUpdate( { ASIN: book.ASIN }, { AuthorityASIN: AuthorityASIN }, function( err, book ){
 				countExec++;
 			} );
