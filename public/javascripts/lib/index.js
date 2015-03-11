05ddb6551f50904522c3e106bfd68a8003a6801b
shinichi-kogai <kogai0121@gmail.com>
2015-03-11 19:02:42 +0900

    フロント側のファイル一式調達

--- public/javascripts/lib/index.js
+++
@@ -1,9 +0,0 @@
-var $ = require('jquery');
-var validate = require('./validate');
-var connection = require('./connection');
-
-$(window).on( 'load' , function(){
-	console.log('loaded');
-	validate.regist;
-	connection();
-});
