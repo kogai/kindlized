fe8598e09afc1f53a8ad8e330b19893cf16220cf
shinichi-kogai <kogai0121@gmail.com>
2015-03-12 13:27:46 +0900

    投稿された書籍の著者がDBに存在するか調べる

--- routes/post/index.js
+++ routes/post/index.js
@@ -1,15 +1,17 @@
 var Q 				= require('q');
+var _ 				= require('underscore');
 var express	 		= require('express');
 var router	  		= express.Router();
 var modelBookList 	= require( '../../shelf/lib/modelBookList' );
+var modelAuthor 	= require( '../../author/lib/modelAuthor' );
 var constant 		= require( '../../common/constant' );
 
 var opHelper				= require( 'apac' ).OperationHelper;
 var makeOpConfig 			= require( '../../common/makeOpConfig' );
 var makeExistenceExpression	= require( './lib/makeExistenceExpression' );
 
-var opConfig 					= new makeOpConfig();
-var opExistenceBook 			= new opHelper( opConfig );
+var opConfig 		= new makeOpConfig();
+var opExistenceBook = new opHelper( opConfig );
 
 var fetchBookListDB = function( data ){
 	var d = Q.defer();
@@ -19,14 +21,17 @@ var fetchBookListDB = function( data ){
 	var titleQuery 	= new RegExp( newBook );
 
 	modelBookList.find( { title: titleQuery }, function( err, bookListInDB ){
+		if( err ) throw err;
+
 		var countBooks = bookListInDB.length;
-		var isRegisterdBook = false;
-		if( countBooks > 0 ){
-			isRegisterdBook = true;
+		data.isNewBook 	= false;
+		if( countBooks === 0 ){
+			data.isNewBook 	= true;
 		}
-		data.isRegisterdBook 	= isRegisterdBook;
-		data.newBook 			= newBook;
-		data.bookListInDB		= bookListInDB;
+
+		data.newBook 		= newBook;
+		data.titleQuery 	= titleQuery;
+		data.bookListInDB	= bookListInDB;
 		d.resolve( data );
 	});
 	return d.promise;
@@ -46,6 +51,9 @@ var fetchBookListAmazon = function( data ){
 					recursionOpExistenceBook();
 				}, constant.interval );
 			}finally{
+				// if isNewAuthor
+				// 新規に著者を登録
+
 				// if( 取得に成功 || 書籍が存在しない ){
 					data.bookListInAmazon = bookListInAmazon;
 					d.resolve( data );
@@ -69,6 +77,36 @@ var sendResponse = function( data ){
 	return d.promise;
 };
 
+var fetchAuthorListDB = function( data ){
+	var d = Q.defer();
+
+	var bookListInDB 			= data.bookListInDB;
+	var authorsRelatedWithBook 	= [];
+
+	for (var i = 0; i < bookListInDB.length; i++) {
+		var authorName = bookListInDB[i].author;
+		for (var j = 0; j < authorName.length; j++) {
+			authorsRelatedWithBook.push( authorName[j] );
+		}
+	}
+
+	authorsRelatedWithBook = _.uniq( authorsRelatedWithBook );
+	modelAuthor.find( { name: { $in: authorsRelatedWithBook } }, function( err, authorsRelatedWithBookInDB ){
+		if( err ) throw err;
+		data.authorsRelatedWithBook 	= authorsRelatedWithBook;
+		data.authorsRelatedWithBookInDB = authorsRelatedWithBookInDB;
+
+		var countAuthors = authorsRelatedWithBookInDB.length;
+		data.isNewAuthor = false;
+		if( countAuthors === 0 ){
+			data.isNewAuthor = true;
+		}
+
+		d.resolve( data );
+	});
+	return d.promise;
+};
+
 router.post( '/', function( req, res ) {
 	Q.when({
 		res : res,
@@ -77,6 +115,7 @@ router.post( '/', function( req, res ) {
 	.then( fetchBookListDB )
 	.then( fetchBookListAmazon )
 	.then( sendResponse )
+	.then( fetchAuthorListDB )
 	.done( function(){
 		console.log('Server-side search books process is completed.');
 	});
