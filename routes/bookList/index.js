759e505aa3fc6a0c68669049f3d6ca41d1e5b368
shinichi-kogai <kogai0121@gmail.com>
2015-03-13 13:01:24 +0900

    rename routes/bookList

---
+++ routes/bookList/index.js
@@ -0,0 +1,31 @@
+var Q 				= require( 'q' );
+var express 		= require('express');
+var router 			= express.Router();
+var modelAuthor 	= require( '../../author/lib/modelAuthor' );
+var modelBookList 	= require( '../../shelf/lib/modelBookList' );
+
+var fetchBookList = function(){
+	var d = Q.defer();
+	var bookList = modelBookList.find( {}, function( err, books ){
+		d.resolve( books );
+	});
+	return d.promise;
+};
+
+var renderRouter = function( books ){
+	var d = Q.defer();
+	console.log( books.length );
+	router.get('/', function(req, res) {
+        res.send( books );
+		d.resolve( books );
+	});
+	return d.promise;
+}
+
+fetchBookList()
+.then( renderRouter )
+.done( function( books ){
+	console.log( 'books router fetched ', books.length );
+});
+
+module.exports = router;
