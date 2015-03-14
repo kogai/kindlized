2b3c5a420a6e1fa91333b3336ee83c0ae27d8bc6
shinichi-kogai <kogai0121@gmail.com>
2015-03-15 00:05:47 +0900

    postMailToUserをpromise化

--- postman/lib/postMailToUser.js
+++ postman/lib/postMailToUser.js
@@ -7,13 +7,17 @@ var modifyNotifiedStatus      = require( 'postman/lib/modifyNotifiedStatus' );
 var Q = require('q');
 
 module.exports = function( user ){
+   var d = Q.defer();
+
    inspectNotifiedBooks( user )
    .then( inspectKindlizedBooks )
    .then( insertMailTemplate )
    .then( sendKindlizedNotification )
    .then( modifyNotifiedStatus )
    .done( function( user ){
-      console.log( 'user.postList ' + user.postList );
       console.log( user._id + 'の処理が完了');
+      d.resolve();
    });
+
+   return d.promise;
 };
