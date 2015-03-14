5e53f6bc46e980b7df346a6b6085dc66ffa132b7
shinichi-kogai <kogai0121@gmail.com>
2015-03-15 00:05:47 +0900

    postMailToUserをpromise化

--- postman/index.js
+++ postman/index.js
@@ -2,20 +2,20 @@ var Q = require('q');
 var fetchUserModel = require( 'postman/lib/fetchUserModel' );
 var postMailToUser = require( 'postman/lib/postMailToUser' );
 
-// module.exports = function(){
+module.exports = function(){
    // *1日に一度実行する
 
-   // 全ての処理を完了
    fetchUserModel()
    .then( function( data ){
-      var d = Q.defer();
 
-      var users = data.users
-      for (var i = 0; i < users.length; i++) {
-         var user = users[i];
-         postMailToUser( user )
-         if( i === users.length - 1 ) d.resolve( data );
-      }
+      var d       = Q.defer();
+      var users   = data.users;
+
+      Q.all( users.map( postMailToUser ) )
+      .done( function(){
+         console.log('done');
+         d.resolve( data );
+      });
 
       return d.promise;
    })
@@ -23,4 +23,4 @@ var postMailToUser = require( 'postman/lib/postMailToUser' );
       console.log( 'postmanの処理が完了' );
    });
 
-// };
+};
