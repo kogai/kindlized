22c033328fc71c5d2f85c21d1f866a67a3bf1a1c
shinichi-kogai <kogai0121@gmail.com>
2015-03-14 13:24:07 +0900

    postman関数を作成

---
+++ postman/index.js
@@ -0,0 +1,16 @@
+module.exports = function(){
+   // *1日に一度実行する
+
+   // 全てのuserモデルを取得
+   // userモデルのリストから各userモデルへ以下の処理を実行
+      // userモデルのbookListからisNotifiedがfalseのものを抽出してpostList配列に格納
+      // postListをメールのテンプレートに挿入
+      // メールを送信
+   // 全ての処理を完了
+
+   var fetchUserModel;
+   var postMailToUser;
+      var inspectNotifiedBooks;
+      var insertMailTemplate;
+      var sendMail;
+};
