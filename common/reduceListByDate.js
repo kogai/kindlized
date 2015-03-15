448d3af0b8a215f1a8b02ec672741801794e28ea
shinichi-kogai <kogai0121@gmail.com>
2015-03-16 01:08:45 +0900

    最終編集日が1日以内のものは処理しない

---
+++ common/reduceListByDate.js
@@ -0,0 +1,20 @@
+var Q					= require('q');
+var moment			= require('moment-timezone');
+var constant		= require( 'common/constant' );
+
+module.exports = function( list ){
+	var d = Q.defer();
+	var siftedList = [];
+	for (var i = 0; i < list.length; i++) {
+
+		var lastModifyTime = list[i].lastModified || list[i]._id.getTimestamp();
+		var todayDate		 = moment();
+		var diffDay			 = todayDate.diff( lastModifyTime, 'days' );
+
+		if( diffDay > constant.periodicalDay ){
+			siftedList.push( list[i] );
+		}
+	}
+	d.resolve( siftedList );
+	return d.promise;
+};
