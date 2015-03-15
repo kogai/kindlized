a89ce356d8c0aac67330f5afbd42d31f8f86b6bf
shinichi-kogai <kogai0121@gmail.com>
2015-03-15 15:22:08 +0900

    shelfに渡す著者リストを最終編集日基準でソート

--- shelf/lib/fetchAuthor.js
+++ shelf/lib/fetchAuthor.js
@@ -5,19 +5,15 @@ module.exports = function( authorList ) {
   // DBから著者リストを非同期に取得する
 	var d = Q.defer();
 
-	ModelAuthor.find().sort({
-		lastModified: -1
-	});
-
-	ModelAuthor.find( {}, function( err, result ) {
-		result.sort({
-			lastModified: 1
-		});
+	ModelAuthor.find( {}).sort({
+		lastModified: 1
+	}).find( {}, function( err, result ){
 		for (var i = 0; i < result.length; i++) {
 			authorList.push(result[i].name);
 		}
 		console.log( authorList.length, '人の著者が登録されている' );
 		d.resolve(authorList);
 	});
+
 	return d.promise;
 };
