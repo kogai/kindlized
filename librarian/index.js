364775c789b903a9ee27729a2302624fad5cab20
shinichi-kogai <kogai0121@gmail.com>
2015-03-16 10:45:58 +0900

    API呼び出し間隔エラーの時にもRelatedItemsを持たない時の処理が走ってたっぽいのを修正

--- librarian/index.js
+++ librarian/index.js
@@ -2,6 +2,15 @@
 // - 書籍DBのkindle化チェック
 // - 書籍DBへの新刊追加
 
+/*
+
+parentsASIN-RelatedItemsのチェックが失敗してるっぽい
+- [ ] シドニアの騎士(10)
+- [ ] 青空エール 3 (マーガレットコミックス)
+- [ ] 姉の結婚 2 (フラワーコミックスアルファ)
+
+*/
+
 var Q               = require('q');
 var regInt          = require('./lib/fetchParentASIN/regInt');
 var fetchBookList   = require('./lib/fetchParentASIN/fetchBookList');
