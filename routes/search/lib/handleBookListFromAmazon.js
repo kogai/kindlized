d5439bb84060d72e896fc6f7d8be940a9d3a41a5
shinichi-kogai <kogai0121@gmail.com>
2015-03-19 20:12:37 +0900

    post/ search/amazonを修正

---
+++ routes/search/lib/handleBookListFromAmazon.js
@@ -0,0 +1,15 @@
+var Q = require('q');
+var modifyAmazonBook = require( 'common/modifyAmazonBook' );
+
+module.exports = function( data ) {
+  var d = Q.defer();
+  var bookListInAmazon = data.bookListInAmazon;
+
+  Q.all( bookListInAmazon.map( modifyAmazonBook ) )
+  .done( function( modBookListForDB ){
+    data.bookListInAmazon = modBookListForDB;
+    d.resolve( data );
+  });
+
+  return d.promise;
+};
