cd00ebcb675c0b02b428620921b27df57ba68936
shinichi-kogai <kogai0121@gmail.com>
2015-03-19 20:12:37 +0900

    post/ search/amazonを修正

---
+++ common/modifyAmazonBook.js
@@ -0,0 +1,36 @@
+module.exports = function( rawBook ){
+  var modBook;
+  var itemAttr;
+  try{
+    itemAttr  = rawBook.ItemAttributes[0];
+
+    /*
+      mongodbはkey名に$を含むオブジェクトを扱えないので
+      文字列化して保存する
+      使う時はJSON.parse()する
+    */
+    var imageSets;
+    if( rawBook.ImageSets ) imageSets = JSON.stringify( rawBook.ImageSets );
+
+    modBook = {
+      status          : 'DEFAULT',
+      ASIN            : rawBook.ASIN,
+      ISBN            : rawBook.ISBN,
+      SKU             : rawBook.SKU,
+      EAN             : itemAttr.EAN,
+
+      author          : itemAttr.Author,
+      title           : itemAttr.Title,
+      publisher       : itemAttr.Publisher,
+      publicationDate : itemAttr.PublicationDate,
+      price           : itemAttr.ListPrice,
+      url             : rawBook.DetailPageURL,
+      images          : imageSets
+    };
+
+  }catch( err ){
+    modBook.status  = 'ERROR';
+  }finally{
+    return rawBook;
+  }
+};
