86407d8331854dbad391f3cf2a12f7b040333329
shinichi-kogai <kogai0121@gmail.com>
2015-03-20 21:19:40 +0900

    [WIP]isnpectBookListに渡っていない書籍がたくさんあるっぽい
    電子化率は10~30%くらいが現状らしい

---
+++ librarian/lib/inspectBookList/fetchBookListNotKindlized.js
@@ -0,0 +1,19 @@
+var Q                = require('q');
+var ModelBookList    = require( 'shelf/lib/modelBookList' );
+var reduceListByDate = require('common/reduceListByDate');
+
+module.exports = function( bookList ) {
+  // DBから著者リストを非同期に取得する
+  var d = Q.defer();
+  console.log('DBから著者リストを非同期に取得する');
+  var query = {
+    AuthorityASIN: /.+/,
+    // AuthorityASIN: { $exists: true },
+    isKindlized: false
+  };
+  ModelBookList.find( query, function( err, result ) {
+      console.log(result.length + '冊の書籍データが存在する');
+      d.resolve( result );
+  });
+  return d.promise;
+};
