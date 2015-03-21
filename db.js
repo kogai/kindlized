db1e824431f3124d85ff76d029280f12e567e39d
shinichi-kogai <kogai0121@gmail.com>
2015-03-21 09:42:51 +0900

    cron処理が走ってなかったのを修正

--- db.js
+++ db.js
@@ -1,45 +1,40 @@
-var librarian   = require( 'librarian' );
-var shelf       = require( 'shelf' );
-var postman     = require( 'postman' );
-var Q           = require( 'q' );
-var cronjob     = require( 'cron' ).CronJob;
-var cronTime    = "0 0 0 * * *";
-var moment      = require( 'moment-timezone' );
+var librarian = require('librarian');
+var shelf = require('shelf');
+var postman = require('postman');
+var Q = require('q');
+var cronjob = require('cron').CronJob;
+var cronTime = "0 0 12 * * *";
+var moment = require('moment-timezone');
 
-var libraryHandler = function( currentTime ){
-    Q.when()
-    .then( shelf )
-    .then( function( bookList ){
-        var d = Q.defer();
-        d.resolve([]);
-        return d.promise;
+var libraryHandler = function(currentTime) {
+  Q.when()
+    .then(shelf)
+    .then(function(bookList) {
+      var d = Q.defer();
+      d.resolve([]);
+      return d.promise;
     })
-    .then( librarian.inspectBookList )
-    .then( librarian.fetchParentASIN )
-    .done( function(){
-      logTime( currentTime );
+    .then(librarian.inspectBookList)
+    .then(librarian.fetchParentASIN)
+    .done(function() {
+      logTime(currentTime);
     });
 };
 
 // タイムゾーンに合わせてログを取る
-var logTime = function( currentTime ){
-	var current = currentTime.tz('Asia/Tokyo').format('YYYY-MM-DD-hA');
-	console.log( 'all process is complete at', current );
+var logTime = function(currentTime) {
+  var current = currentTime.tz('Asia/Tokyo').format('YYYY-MM-DD-hA');
+  console.log('all process is complete at', current);
 };
 
 //定期実行
 job = new cronjob({
-	cronTime : cronTime,
-	onTick: function () {
-      libraryHandler( moment() );
-      postman();
-	},
-	start: false
+  cronTime: cronTime,
+  onTick: function() {
+    libraryHandler(moment());
+    postman();
+  },
+  start: false
 });
 
-
-module.exports = function(){
-    job.start();
-};
-
-libraryHandler( moment() );
+job.start();
