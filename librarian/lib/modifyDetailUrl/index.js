6e8616e095fa0d04a0412ab502b0a9a66cacf893
shinichi-kogai <kogai0121@gmail.com>
2015-03-21 12:11:28 +0900

    url書き換え処理

--- librarian/lib/modifyDetailUrl/index.js
+++ librarian/lib/modifyDetailUrl/index.js
@@ -1,18 +1,25 @@
-var kindlizedBookList = require('librarian/lib/modifyDetailUrl/kindlizedBookList');
 var Q = require('q');
+var kindlizedBookList = require('librarian/lib/modifyDetailUrl/kindlizedBookList');
+var lookUpAuthority = require('librarian/lib/modifyDetailUrl/lookUpAuthority');
+var lookUpEbooks = require('librarian/lib/modifyDetailUrl/lookUpEbooks');
+var modifyUrl = require('librarian/lib/modifyDetailUrl/modifyUrl');
 
 module.exports = function() {
   var d = Q.defer();
   kindlizedBookList()
-  // lookUpEbook
-  // modifyDetailUrl
+  .then(lookUpAuthority)
+  .then(lookUpEbooks)
+  .then(modifyUrl)
+  // modifyUrl
   // URLを書き換え
   // 書き換え済みフラグを立てる
   .done(function( books ){
-    var titles = books.map( function( book ){
-      return book.title;
-    });
-    console.log( titles );
+      var titles = books.map( function( book ){
+        // return book.title;
+        return book.ebookASIN + ':' + book.title;
+        // return book;
+      });
+      console.log( titles );
     console.log('URLを書き換え完了');
     d.resolve();
   });
