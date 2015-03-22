26e45f3adcf674b10bce4d44d5406caf1bd9cdd7
shinichi-kogai <kogai0121@gmail.com>
2015-03-22 23:51:55 +0900

    計画

--- librarian/lib/fetchParentASIN/fetchBookList.js
+++ librarian/lib/fetchParentASIN/fetchBookList.js
@@ -1,16 +1,15 @@
-var Q                = require('q');
-var ModelBookList    = require( 'shelf/lib/modelBookList' );
+var Q = require('q');
+var ModelBookList = require('shelf/lib/modelBookList');
 var reduceListByDate = require('common/reduceListByDate');
 
 module.exports = function() {
-  // DBから著者リストを非同期に取得する
   var d = Q.defer();
-  console.log('DBから著者リストを非同期に取得する');
-  ModelBookList.find( {
-    AuthorityASIN: { $not: /.+/  }
-  }, function( err, result ) {
-      console.log(result.length + '冊の書籍データが存在する');
-      d.resolve( result );
+  ModelBookList.find({
+    AuthorityASIN: {
+      $not: /.+/
+    }
+  }, function(err, haveNotAuthorityAsin) {
+    d.resolve(haveNotAuthorityAsin);
   });
   return d.promise;
 };
