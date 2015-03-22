95e6c0768ff544e62c0118494c25ddd86f892019
shinichi-kogai <kogai0121@gmail.com>
2015-03-22 16:53:34 +0900

    bookIdがnullで保存される時が/amazon-api検索直後の擬似modelにはidがない
    db.collectionにsave後でないとメール登録用のidが取れないので
    save後にレスポンスを送る

--- routes/search/lib/saveBookListToDB.js
+++ routes/search/lib/saveBookListToDB.js
@@ -1,16 +1,28 @@
 var Q = require('q');
+var _ = require('underscore');
 
 var modelBookList = require('shelf/lib/modelBookList');
-var saveBook      = require('common/saveBook');
+var saveBook = require('common/saveBook');
 
-module.exports = function( data ) {
+module.exports = function(data) {
   var d = Q.defer();
   var bookListInAmazon = data.bookListInAmazon;
 
-  Q.all( bookListInAmazon.map( saveBook ) )
-  .done( function(){
-     d.resolve( data );
-  });
+  Q.all(
+      bookListInAmazon.map(function(book) {
+        var def = Q.defer();
+        saveBook(book)
+          .done(function(book) {
+            def.resolve(book);
+          });
+        return def.promise;
+      })
+    )
+    .done(function(savedBooks) {
+      savedBooks = _.compact( savedBooks );
+      data.savedBooks = savedBooks;
+      d.resolve(data);
+    });
 
   return d.promise;
 };
