554313e3d4919879b00ab04d7aba722d25318e13
shinichi-kogai <kogai0121@gmail.com>
2015-03-23 13:16:29 +0900

    書籍にASINが無い場合の安全弁

--- common/saveBook.js
+++ common/saveBook.js
@@ -1,29 +1,32 @@
 var ModelBookList = require('shelf/lib/modelBookList.js');
+var log = require('common/log');
 
 module.exports = function( book ) {
   ModelBookList.findOne({
     ASIN: book.ASIN
   }, function( err, dbBook ) {
     if (!dbBook) {
-
-      var newBook = new ModelBookList({
-        status: book.satus,
-        ASIN: book.ASIN,
-        EAN: book.EAN,
-        author: book.author,
-        title: book.title,
-        publisher: book.publisher,
-        publicationDate: book.publicationDate,
-        price: book.price,
-        url: book.url,
-        images: book.images,
-        isKindlized: false
-      });
-
-      newBook.save(function( err ) {
-        if (err) console.log(err);
-        console.log( '書籍:' + newBook.title + 'が登録されました'　);
-      });
+      try{
+        var newBook = new ModelBookList({
+          status: book.satus,
+          ASIN: book.ASIN,
+          EAN: book.EAN,
+          author: book.author,
+          title: book.title,
+          publisher: book.publisher,
+          publicationDate: book.publicationDate,
+          price: book.price,
+          url: book.url,
+          images: book.images,
+          isKindlized: false
+        });
+        newBook.save(function( err ) {
+          if (err) console.log(err);
+          log.info( '書籍:' + newBook.title + 'が登録されました'　);
+        });
+      }catch(error){
+        log.info(error);
+      }
     }
   });
 };
