d7b9f94414a72b51f56bb701c5f806db6730a003
shinichi-kogai <kogai0121@gmail.com>
2015-03-25 02:18:38 +0900

    kindlizedの定義を修正

--- common/saveBook.js
+++ common/saveBook.js
@@ -21,7 +21,7 @@ module.exports = function( book ) {
             price: book.price,
             url: book.url,
             images: book.images,
-            isKindlized: false,
+            isKindlized: dbBook.isKindlized,
             AuthorityASIN: book.AuthorityASIN
           });
           newBook.save(function( err ) {
