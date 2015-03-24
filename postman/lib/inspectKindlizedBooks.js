f04bd81f556d597dcb7d055bf8f9cc7567f7e637
shinichi-kogai <kogai0121@gmail.com>
2015-03-24 12:52:52 +0900

    postmanのログを取る

--- postman/lib/inspectKindlizedBooks.js
+++ postman/lib/inspectKindlizedBooks.js
@@ -1,24 +1,34 @@
 // userモデルのbookListからkindle化されているものを抽出してpostList配列に格納
 
-var  Q = require('q');
-var modelBookList = require( 'shelf/lib/modelBookList' );
+var Q = require('q');
+var _ = require('underscore');
+var modelBookList = require('shelf/lib/modelBookList');
+var logPostman = require('common/logEx').postman;
 
-module.exports = function( user ){
-   var d = Q.defer();
+module.exports = function(user) {
+	var d = Q.defer();
 
-   var postList      = user.postList;
-   var kindlizedList = [];
+	var postList = user.postList;
+	var kindlizedList = [];
 
-   modelBookList.find( { _id: { $in: postList } }, function( err, books ){
-      for (var i = 0; i < books.length; i++) {
-         var book = books[i];
-         if( book.isKindlized ){
-            kindlizedList.push( book );
-         }
-      }
-      user.kindlizedList = kindlizedList;
-      d.resolve( user );
-   });
+	modelBookList.find({
+		_id: {
+			$in: postList
+		}
+	}, function(err, books) {
+		logPostman.info('\n', books.length);
 
-   return d.promise;
+
+		user.kindlizedList = books.map(function(book) {
+			if (book.isKindlized === true) {
+				return book;
+			}
+		});
+
+		logPostman.info('\n', user.kindlizedList);
+    user.kindlizedList = _.compact(user.kindlizedList);
+		d.resolve(user);
+	});
+
+	return d.promise;
 };
