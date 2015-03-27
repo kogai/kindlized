f755ee14f29428b3e55b5986aad89d15273e0520
shinichi-kogai <kogai0121@gmail.com>
2015-03-27 21:55:56 +0900

    検索条件を修正

--- librarian/lib/fetchParentASIN/index.js
+++ librarian/lib/fetchParentASIN/index.js
@@ -2,6 +2,7 @@ var Q = require('q');
 var fetchBookList = require('librarian/lib/fetchParentASIN/fetchBookList');
 var handlePromiseSerialize = require('librarian/lib/fetchParentASIN/handlePromiseSerialize');
 var updateBooks = require('librarian/lib/fetchParentASIN/updateBooks');
+var logWrap = require('common/logWrap')( 'librarian', true );
 
 module.exports = function(){
   var defferd = Q.defer();
@@ -9,8 +10,17 @@ module.exports = function(){
   .then(fetchBookList)
   .then(handlePromiseSerialize)
   .then(updateBooks)
+  .fail(erroHandling)
   .done(function(){
+    logWrap.info('AuthorityASINの調査処理が完了');
     defferd.resolve();
   });
   return defferd.promise;
+};
+
+var erroHandling = function(){
+  console.log('AuthorityASINのエラーハンドリング');
+  var d = Q.defer();
+  d.resolve();
+  return d.promise;
 };
