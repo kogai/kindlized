6db80b0138d93aaa4ef1e1f9f15abd58794ced27
shinichi-kogai <kogai0121@gmail.com>
2015-03-30 21:20:36 +0900

    updateAuthors 著者情報を更新

---
+++ librarian/lib/inspectNewRelease/updateAuthors.js
@@ -0,0 +1,38 @@
+var Q = require('q');
+var modelAuthor = require('author/lib/modelAuthor');
+
+module.exports = function (authors) {
+  "use strict";
+  var d = Q.defer();
+
+	Q.all(
+		authors.map(function (author) {
+			var def = Q.defer();
+
+			var query = {
+				_id: author._id
+			};
+			var modify = {
+				wroteBooks: author.wroteBooks
+			};
+			var callback = function (error, author) {
+				if(error){
+					def.reject(error);
+				}else{
+					def.resolve(author);
+				}
+			};
+			modelAuthor.findOneAndUpdate(query, modify, callback);
+
+			return def.promise;
+		})
+	)
+	.fail(function (error) {
+		d.reject(error);
+	})
+	.done(function (modAuthors) {
+		d.resolve(modAuthors);
+	});
+
+  return d.promise;
+};
