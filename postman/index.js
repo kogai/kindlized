156e7329b041c45b0bc72d71e68f809b1397cc6d
shinichi-kogai <kogai0121@gmail.com>
2015-04-02 16:33:37 +0900

    新刊調査処理をpostmanの直前に変更 && linting

--- postman/index.js
+++ postman/index.js
@@ -2,13 +2,16 @@ var Q = require('q');
 
 var fetchUserModel = require( 'postman/lib/fetchUserModel' );
 var postMailToUser = require( 'postman/lib/postMailToUser' );
+var inspectNewRelease = require('librarian/lib/inspectNewRelease');
 var logPostman = require('common/logEx').postman;
 
 module.exports = function(){
   "use strict";
    // *1日に一度実行する
-   fetchUserModel()
-   .then( function( data ){
+  Q.when()
+    .then(inspectNewRelease)
+    .then(fetchUserModel)
+    .then( function( data ){
 
       var d       = Q.defer();
       var users   = data.users;
@@ -21,7 +24,7 @@ module.exports = function(){
 
       return d.promise;
    })
-   .done( function( data ){
+   .done( function(){
       logPostman.info( 'postmanの処理が完了' );
    });
 };
