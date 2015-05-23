d3889773d44568a76c69691cf4670586a33c9827
shinichi-kogai <kogai0121@gmail.com>
2015-05-24 00:30:02 +0900

    著者ページからのメール配信登録

--- routes/save/index.js
+++ routes/save/index.js
@@ -10,31 +10,47 @@ var saveUserBookList = require('routes/save/lib/saveUserBookList');
 
 var isNewAuthor = require('routes/save/lib/isNewAuthor');
 var saveUserAuthorList = require('routes/save/lib/saveUserAuthorList');
+var BookList = require('models/BookList');
+var log = require('common/log');
+
+var saveBook = function(req, res, newBook){
+	// temp
+	req.session.passport = {
+		user: "55098ed6c0fa27f716c0717e"
+	};
+
+	Q.when({
+		req: req,
+		res: res,
+		newBook: newBook
+	})
+	.then(isNewBook)
+	.then(saveBookList)
+	.then(saveUserBookList)
+	.then(isNewAuthor)
+	.then(saveUserAuthorList)
+	.done(function() {
+		res.send({
+			newBook: newBook
+		});
+	});
+};
 
 router.post('/', function(req, res) {
-	console.log("req.body", req.body);
 	var payload = req.body;
 	var newBook = payload.newBook;
 
 	if(payload.isAuthorPage){
-		console.log("payload.ASIN", payload.ASIN);
-		res.send('ok');
-	}else{
-		Q.when({
-			req: req,
-			res: res,
-			newBook: newBook
-		})
-		.then(isNewBook)
-		.then(saveBookList)
-		.then(saveUserBookList)
-		.then(isNewAuthor)
-		.then(saveUserAuthorList)
-		.done(function() {
-			res.send({
-				newBook: newBook
-			});
+		BookList.findOne({
+			ASIN: payload.ASIN
+		}, function(err, book){
+			if(err){
+				return console.log(err);
+			}
+			saveBook(req, res, book);
 		});
+	}else{
+		saveBook(req, res, newBook);
 	}
 });
 
