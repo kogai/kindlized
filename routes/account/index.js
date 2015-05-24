da1a1c32927c05b225c1276c65f245bb8ff57cfa
shinichi-kogai <kogai0121@gmail.com>
2015-05-24 12:29:58 +0900

    こんな著者の書籍がKindle化されていますの部分のクエリを改善
    8s前後かかっていたのを2s以下でレスポンスを返すように

--- routes/account/index.js
+++ routes/account/index.js
@@ -1,35 +1,34 @@
-var Q 		= require( 'q' );
+var Q = require('q');
 var express = require('express');
-var router 	= express.Router();
-var verify 	= require( 'routes/account/verify' );
-var regist 	= require( 'routes/account/regist' );
-var login 	= require( 'routes/account/login' );
+var router = express.Router();
+var verify = require('routes/account/verify');
+var regist = require('routes/account/regist');
+var login = require('routes/account/login');
 var localPassport = login.localPassport;
 var Author = require('models/Author');
 
-router.get( '/login/success', function( req, res ){
+router.get('/login/success', function(req, res) {
 	"use strict";
-	res.redirect( 303, '/' );
+	res.redirect(303, '/');
 });
 
 router.post(
 	'/login',
 	localPassport.authenticate(
-		'local',
-		{
+		'local', {
 			successRedirect: '/',
 			failureRedirect: '/account/fail'
 		}
 	)
 );
 
-router.post( '/logout', function( req, res ){
+router.post('/logout', function(req, res) {
 	"use strict";
 	delete req.session.passport.user;
-	res.send( 'ログアウト完了しました。' );
+	res.send('ログアウト完了しました。');
 });
 
-router.get('/verify', function( req, res ) {
+router.get('/verify', function(req, res) {
 	"use strict";
 	verify({
 		res: res,
@@ -37,7 +36,7 @@ router.get('/verify', function( req, res ) {
 	});
 });
 
-router.post('/regist', function( req, res ) {
+router.post('/regist', function(req, res) {
 	"use strict";
 	regist({
 		res: res,
@@ -45,27 +44,20 @@ router.post('/regist', function( req, res ) {
 	});
 });
 
-router.get('/', function( req, res ) {
+router.get('/', function(req, res) {
 	"use strict";
-	Author.find({}, function(err, authors){
-		if(err){
-			console.log(err);
-			return err;
-		}
-		res.render( 'account', {
-			title: 'アカウント登録 | ログイン',
-			authorLinks: (function(authors){
-				var rand = Math.floor(Math.random() * (authors.length - 1) + 1 );
-				var limitedAuthor = [];
-				var i;
-				if(rand + 12 >= authors.length ){
-					rand -= 12;
-				}
-				for (i = rand; i < rand + 12; i++) {
-					limitedAuthor.push(authors[i]);
-				}
-				return limitedAuthor;
-			}(authors))
+
+	var queryAuthorCount = Author.find().count();
+	queryAuthorCount.exec(function(err, count){
+
+		var randomSkip = Math.random() * (count - 1);
+		var queryAuthors = Author.find({}).skip(randomSkip).limit(12);
+
+		queryAuthors.exec(function(err, authors){
+			res.render("account", {
+				title: 'アカウント登録 | ログイン',
+				authorLinks: authors
+			});
 		});
 	});
 });
