61ef26d3b3fa86d3e9dd58e4ce542d99505cb14e
shinichi-kogai <kogai0121@gmail.com>
2015-05-24 17:03:47 +0900

    fetchPageCountをリファクタリング
    エラーの表示をわかりやすくした

--- shelf/lib/updateAuthorModifiedTime.js
+++ shelf/lib/updateAuthorModifiedTime.js
@@ -4,7 +4,7 @@ var Q = require('q');
 var moment = require('moment-timezone');
 
 var log = require('common/log');
-// var Author = require('models/Author');
+var Author = require('models/Author');
 
 module.exports = function(author) {
 	// 著者リストの最終編集日をアップデート
@@ -14,12 +14,12 @@ module.exports = function(author) {
   var query = { name: author.name };
   var update = { lastModified: modifiedTime };
 
-	// Author.findOneAndUpdate(query, update, function(err, author) {
-  //   if(err){
-  //     return log.info(err);
-  //   }
+	Author.findOneAndUpdate(query, update, function(err, author) {
+    if(err){
+      return log.info(err);
+    }
 		d.resolve(author);
-	// });
+	});
 
 	return d.promise;
 };
