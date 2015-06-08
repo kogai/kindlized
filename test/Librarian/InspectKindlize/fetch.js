cc4c1995b81797b2efc3d1cdc88d03d11efa991e
shinichi-kogai <kogai0121@gmail.com>
2015-06-08 20:59:19 +0900

    InspectKindlizeのスペック

--- test/Librarian/InspectKindlize/fetch.js
+++ test/Librarian/InspectKindlize/fetch.js
@@ -1,8 +1,10 @@
 "use strict";
 
 require('should');
+var moment = require('moment-timezone');
 
 var InspectKindlize = require('Librarian/InspectKindlize');
+var PERIODICAL_DAY = require('common/constant').PERIODICAL_DAY;
 var Books;
 
 describe('AuthorityASINのある書籍を500冊取得する', function(){
@@ -23,4 +25,34 @@ describe('AuthorityASINのある書籍を500冊取得する', function(){
 		(Books.length).should.be.below(501);
 	});
 
+	it('AuthorityASINを持っている', function(){
+		var result = true, i;
+		for (i = 0; i < Books.length; i++) {
+			if(!Books[i].AuthorityASIN){
+				result = false;
+			}
+		}
+		result.should.be.exactly(true);
+	});
+
+	it('kindle化された書籍は含まれない', function(){
+		var result = true, i;
+		for (i = 0; i < Books.length; i++) {
+			if(Books[i].isKindlized){
+				result = false;
+			}
+		}
+		result.should.be.exactly(true);
+	});
+
+	it('1日以上前に編集された書籍のみ', function(){
+		var result = true, i, yesterday = moment().subtract(PERIODICAL_DAY, 'days'), modifiedDay;
+		for (i = 0; i < Books.length; i++) {
+			modifiedDay = Books[i].modifiedLog.InspectKindlizeAt;
+			if(moment(modifiedDay).isAfter(yesterday)){
+				result = false;
+			}
+		}
+		result.should.be.exactly(true);
+	});
 });
