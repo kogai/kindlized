9fc3bcb4ca701e04fcae5074e963b9a7a6f746f1
shinichi-kogai <kogai0121@gmail.com>
2015-06-08 10:30:13 +0900

    Librarianはkindle化された書籍をTwitterBotに通知する

--- librarian/lib/inspectBookList/index.js
+++ librarian/lib/inspectBookList/index.js
@@ -1,4 +1,7 @@
+"use strict";
+
 var Q = require('q');
+
 var fetchBookListNotKindlized = require('librarian/lib/inspectBookList/fetchBookListNotKindlized');
 var inspectBook = require('librarian/lib/inspectBookList/inspectBook');
 var modifyDetailUrl = require('librarian/lib/modifyDetailUrl');
@@ -9,7 +12,7 @@ module.exports = function() {
   fetchBookListNotKindlized()
     .then(inspectBook)
     .done(function(bookList) {
-      log.info(bookList.length, 'inspectBookListの処理を完了');
+      log.info(bookList.length + '冊のkindle化調査処理を完了');
       d.resolve();
     });
   return d.promise;
