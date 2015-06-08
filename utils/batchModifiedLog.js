d4b76f33c651496ce69cbcbcca46aed8fe354f4a
shinichi-kogai <kogai0121@gmail.com>
2015-06-08 20:21:21 +0900

    変数名を一般性のあるものに修正

--- utils/batchModifiedLog.js
+++ utils/batchModifiedLog.js
@@ -6,13 +6,17 @@ var Q = require('q');
 var BookList = require('models/BookList');
 var LIMIT = process.argv[2];
 var current = moment("2015-06-01");
+var log = require('common/log');
 
 function batchModifiedLog(opts){
 	if(!opts.limit) { throw new Error("limitの指定が無い"); }
-	if(!opts.modifier) { throw new Error("編集したい内容が無い"); }
+	if(!opts.conditions) { throw new Error("検索条件が無い"); }
+	if(!opts.update) { throw new Error("編集したい内容が無い"); }
+	if(!opts.Model) { throw new Error("編集したいCollectionが無い"); }
 
 	this.limit = opts.limit;
-	this.modifier = opts.modifier;
+	this.conditions = opts.conditions;
+	this.update = opts.update;
 	this.Model = opts.Model;
 	this.index = 0;
 }
@@ -32,11 +36,11 @@ batchModifiedLog.prototype.recursive = function(maxBook){
 
 	query.exec(function(err, books){
 		if(err){
-			return console.log("err", err);
+			return log.info("err", err);
 		}
 
 		if((_self.index + 1) * LIMIT > maxBook){
-			return console.log("Recursive process end.");
+			return log.info("Recursive process end.");
 		}
 
 		_self.update(books);
@@ -44,24 +48,22 @@ batchModifiedLog.prototype.recursive = function(maxBook){
 	});
 };
 
-batchModifiedLog.prototype.update = function(books){
+batchModifiedLog.prototype.update = function(collections){
 	var _self = this;
-	Q.all(books.map(function(book){
-		_self.Model.findOneAndUpdate({ ASIN: book.ASIN }, _self.modifier, function(err, book){
+	Q.all(collections.map(function(collection){
+		_self.Model.findOneAndUpdate({ ASIN: collection.ASIN }, _self.update, function(err, collection){
 			if(err){
-				return console.log(err);
+				return log.info(err);
 			}
-			console.log(book.title);
+			log.info(collection.title);
 		});
-	}))
-	.done(function(){
-		console.log("done.");
-	});
+	}));
 };
 
 var Batch = new batchModifiedLog({
 	limit: LIMIT,
-	modifier: {
+	conditions: {},
+	update: {
 		modifiedLog: {
 			AddBookAt: current,
 			InspectKindlizeAt: current,
