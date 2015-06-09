7b651a0db330399556eb568c744bf25e0b355ffd
shinichi-kogai <kogai0121@gmail.com>
2015-06-09 10:56:19 +0900

    librarianを削除
    サービスはNameLikeThisとしたい

--- librarian/lib/inspectBookList/makeInspectExpression.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(ASIN) {
-	// 検索条件オブジェクトのコンストラクター
-	this.ItemId            = ASIN;
-	this.RelationshipType  = 'AuthorityTitle';
-	this.ResponseGroup     = 'RelatedItems, Small';
-};
