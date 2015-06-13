7f91d2017e3db18614ac84c037cd94d2a8ae38a9
shinichi-kogai <kogai0121@gmail.com>
2015-06-13 10:43:14 +0900

    更新を保存するときのfind条件が一意になっていなかった

--- Librarian/Librarian.js
+++ Librarian/Librarian.js
@@ -111,7 +111,7 @@ Librarian.prototype.lookup = function(book){
 **/
 Librarian.prototype.update = function(book, update, callback){
 	var conditions = {
-		ASIN: book.ASIN[0]
+		_id: book._id
 	};
 
 	if(update.modifiedLog === undefined){
