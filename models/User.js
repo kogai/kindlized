0f2b5d707dc115a3c1c9da27fd7dd68cd4d119be
shinichi-kogai <kogai0121@gmail.com>
2015-06-18 21:29:06 +0900

    Userモデルを1つのファイルにまとめる

--- models/User.js
+++ models/User.js
@@ -2,6 +2,12 @@
 
 var mongoose = require('mongoose');
 var mongodb = require('common/makeCredential')('mongodb');
+var db = mongoose.createConnection(mongodb);
+
+var Q = require('q');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = require('common/constant').SALT_WORK_FACTOR;
+var Utils = require('common/Utils');
 
 /**
 @example
@@ -20,14 +26,73 @@ var mongodb = require('common/makeCredential')('mongodb');
   }]
 }
 **/
-var modelUser = require('models/lib/modelUser');
-var comparePassword = require('models/lib/comparePassword');
-var hashPassword = require('models/lib/hashPassword');
 
-modelUser.methods.comparePassword = comparePassword;
-modelUser.pre('save', hashPassword);
+var UserSchema = new mongoose.Schema({
+  mail: {
+    type: String,
+    index: {
+      unique: true
+    }
+  },
+  password: String,
+  verifyId: String,
+  isVerified: Boolean,
+  bookList: Array,
+  seriesList: Array,
+	modifiedLog: {
+		seriesListAt: Date
+	}
+});
 
-var db = mongoose.createConnection(mongodb);
-var newModel = db.model('User', modelUser);
+UserSchema.methods.comparePassword = function(candidatePassword, hashedPassword, done) {
+  bcrypt.compare(candidatePassword, hashedPassword, function(err, isMatch) {
+    if (err) {
+      return done(err);
+    }
+    done(null, isMatch);
+  });
+};
 
-module.exports = newModel;
+
+
+UserSchema.pre('save', function(next) {
+	var _user = this;
+
+	// only hash the password if it has been modified (or is new)
+	if (!_user.isModified('password')){
+    return next();
+  }
+
+	var generateSalt = function() {
+		var d = Q.defer();
+		bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+			if(err){
+        return next(err);
+      }
+
+			d.resolve(salt);
+		});
+		return d.promise;
+	};
+
+	var hashPassword = function(salt) {
+		var d = Q.defer();
+		bcrypt.hash(_user.password, salt, function(err, hash) {
+			if (err){
+        return next(err);
+      }
+
+			_user.password = hash;
+			d.resolve(hash);
+		});
+		return d.promise;
+	};
+
+	generateSalt()
+	.then(hashPassword)
+	.done(function(hash) {
+		next();
+	});
+});
+
+module.exports = db.model('user', UserSchema);
