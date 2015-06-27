ed20fc818aa595ed8f870375b71b254647a3a9e5
shinichi-kogai <kogai0121@gmail.com>
2015-06-27 14:09:39 +0900

    Userクラスにプロフィール編集メソッドを追加

--- common/User.js
+++ common/User.js
@@ -1,14 +1,14 @@
-"use strict";
+"use strict"
 
-const _ = require('underscore');
-const log = require('common/log');
+const _ = require('underscore')
+const log = require('common/log')
 
 class User {
 	constructor(userId){
-		this.userId = userId;
-		this.Series = require('Librarian/Series');
-		this.UserCollections = require('models/User');
-		this.BookCollections = require('models/Book');
+		this.userId = userId
+		this.Series = require('Librarian/Series')
+		this.UserCollections = require('models/User')
+		this.BookCollections = require('models/Book')
 	}
 
 	/**
@@ -16,38 +16,38 @@ class User {
 	@param { Function } done - 完了後に呼ばれるコールバック関数
 	**/
 	saveBook(book, done){
-		let _self = this;
-		let conditions = { _id: this.userId };
+		let _self = this
+		let conditions = { _id: this.userId }
 		let newBook = {
 			bookId: book._id,
 			title: book.title,
 			isNotified: false
-		};
+		}
 		let updates = {
 			$push: {
 				bookList: newBook
 			}
-		};
+		}
 		let options = {
 			upsert: true
-		};
+		}
 
 		this.UserCollections.findOne(conditions, function(err, user){
 			if(err){
-				return done(err);
+				return done(err)
 			}
 
 			if(_.where(user.bookList, newBook).length > 0){
-				return done(null, 'この書籍は登録済みです。');
+				return done(null, 'この書籍は登録済みです。')
 			}
 
 			_self.UserCollections.findOneAndUpdate(conditions, updates, options, function(err, savedUser){
 				if(err){
-					return done(err);
+					return done(err)
 				}
-				done(null, savedUser);
-			});
-		});
+				done(null, savedUser)
+			})
+		})
 	}
 
 	reduceBook(book, done){
@@ -65,8 +65,31 @@ class User {
 	fetchRegisteredSeries(){
 
 	}
+
+
+	/**
+	@param { String } property - 編集するプロパティ名
+	@param { String | Array | Object } body - 編集するデータ
+	@param { Function } done - 完了後に呼ばれるコールバック関数
+	**/
+	modifiyProfile(property, body, done){
+		let conditions = { _id: this.userId }
+		let updates = {}
+		updates[property] = body
+		let options = {
+			upsert: true
+		}
+
+		this.UserCollections.findOneAndUpdate(conditions, updates, options, function(err, savedUser){
+			if(err){
+				return done(err)
+			}
+			done(null, savedUser)
+		});
+
+	}
 }
 
 module.exports = function(userId){
-	return new User(userId);
-};
+	return new User(userId)
+}
