cec51941993cabe7f1520da1611c5f89fe186018
Shinichi Kogai <kogai0121@gmail.com>
2015-10-12 11:40:18 +0900

    [fix] lint

--- itamae.rb
+++ itamae.rb
@@ -45,32 +45,32 @@ end
 
 # execute 'Install iojs' do
 #   user "root"
-#   command "nvm install iojs-v2.3.0 && nvm alias default iojs-v2.3.0"
+#   command "nvm install v4.1.2 && nvm alias default v4.1.2"
 # end
 
-remote_file "/home/vagrant/.bash_profile" do
-  owner "root"
-  group "root"
-  source "conf/.bash_profile_development"
+remote_file '/home/vagrant/.bash_profile' do
+  owner 'root'
+  group 'root'
+  source 'conf/.bash_profile_development'
   only_if 'test -d /home/vagrant/'
   not_if 'test -e /home/vagrant/.bash_profile'
 end
 
-remote_file "/root/.bash_profile" do
-  owner "root"
-  group "root"
-  source "conf/.bash_profile"
+remote_file '/root/.bash_profile' do
+  owner 'root'
+  group 'root'
+  source 'conf/.bash_profile'
   only_if 'test -d /root/'
   not_if 'test -e /root/.bash_profile'
 end
 
 # execute 'create DB and User' do
-#   user "root"
+#   user 'root'
 #   command "mongo /vagrant/conf/mongo.js"
 #   only_if "mongo"
 # end
 #
 # execute 'import' do
-#   command "mongorestore -h 127.0.0.1:27017 -d kindlized -u <user> -p <password> /vagrant/backups/kindlized"
+#   command 'mongorestore -h 127.0.0.1:27017 -d kindlized -u <user> -p <password> /vagrant/backups/kindlized'
 #   only_if "mongo"
 # end
