89fbe05b0caffa8ed36b368df0b6756684783b41
Shinichi Kogai <kogai0121@gmail.com>
2015-10-12 22:47:50 +0900

    [change] シリーズ配信クラスに各メソッド追加

--- Postman/PostSeries.js
+++ Postman/PostSeries.js
@@ -1,18 +1,94 @@
-/*
-1. Seriesコレクションから新刊を持った(hasNewReleaseな)シリーズ配列を取得
-2. [1]から[{
-  seriesId, // シリーズID
-  newReleases, // 新刊書籍IDの配列
-}]を生成
-2.1. [1]から[...seriesId]を生成
-3. 各ユーザーに対して処理を開始
-4. UserコレクションのSeriesListプロパティに$in: [...seriesId]なシリーズを取得
-5. [2]からBookコレクションを経由して新刊書籍リストを生成
-6. メール配信処理
-*/
-class PostSeries {
+import Mailer from 'common/Mailer';
+import SeriesModel from 'models/Series';
 
+function filterNewReleases(src, dist, props) {
+  /* Series.currentContainsとSeries.recentContainsの差分配列を生成 */
+  return dist.filter((item)=> {
+    let isDiff = true;
+    src.filter((distItem)=> {
+      if (item[props].toString() === distItem[props].toString()) {
+        isDiff = false;
+      }
+      return item[props].toString() !== distItem[props].toString();
+    });
+    return isDiff ? item : null;
+  });
 }
 
+function createSeriesReleases(seriesList) {
+  return seriesList.map((series)=> {
+    return {
+      id: series.id,
+      newReleasesCount: series.currentCount - series.recentCount,
+      newReleases: filterNewReleases(series.recentContains, series.currentContains, '_id'),
+    };
+  });
+}
+
+function createSeriesIDs(seriesList) {
+  return seriesList.map(series => series.id);
+}
+
+function createNewBookIDs(newReleases) {
+  const bookList = [];
+  newReleases.forEach((item)=> {
+    item.newReleases.forEach((book)=> {
+      bookList.push(book._id);
+    });
+  });
+  return bookList;
+}
+
+class PostSeries {
+  constructor() {
+    this.seriesReleases = [];
+    this.seriesIDs = [];
+  }
+
+  getHasNewReleases(done) {
+    const conditions = {
+      hasNewRelease: true,
+    };
+    SeriesModel.find(conditions, (err, series)=> {
+      if (err) {
+        return done(err);
+      }
+      this.seriesReleases = createSeriesReleases(series);
+      this.seriesIDs = createSeriesIDs(series);
+      done(null);
+    });
+  }
+
+  getNewBookIDs(user, done) {
+    const seriesList = user.seriesList;
+    const seriesReleases = this.seriesReleases;
+
+    const notifyList = seriesReleases.filter((item)=> {
+      let notifyItem;
+      seriesList.forEach((series)=> {
+        if (series._id.toString() === item.id.toString()) {
+          notifyItem = item;
+        }
+      });
+      return notifyItem;
+    });
+    done(null, createNewBookIDs(notifyList));
+  }
+
+  sendMail(user, books, done) {
+    const mail = Mailer({ to: user.mail });
+    mail.createTemplate('series', books, (err, templates)=> {
+      if (err) {
+        done(err);
+      }
+      const from = 'info@kindlized.it';
+      const to = user.mail;
+      const subject = '[kindlize.it] 新刊通知';
+      mail.setMail(from, to, subject, templates);
+      mail.send(done);
+    });
+  }
+}
+
+// クラスのエクスポート
 export default PostSeries;
-export const postSeries = new PostSeries();
