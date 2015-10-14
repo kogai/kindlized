43cc6c7cb0b3de00d0776bb9c0ed3a365c29e111
Shinichi Kogai <kogai0121@gmail.com>
2015-10-14 09:01:42 +0900

    [change] テストのモック化

--- test/routes/author/index.spec.js
+++ test/routes/author/index.spec.js
@@ -1,19 +1,31 @@
 import assert from 'power-assert';
 import request from 'supertest';
 import app from 'app.js';
+import AutherModel from 'models/Author';
 
-describe('routes:author', ()=> {
+describe('/routes/author', ()=> {
   let response;
+
   before((done)=> {
-    request(app)
-      .get('/author/1000')
-      .end((err, ret)=> {
-        if (err) {
-          return done(err);
-        }
-        response = ret;
-        done();
-      });
+    const author = new AutherModel({
+      name: 'テスト',
+      lastModified: new Date(),
+    });
+
+    author.save((saveError)=> {
+      if (saveError) {
+        return done(saveError);
+      }
+      request(app)
+        .get('/author/1')
+        .end((err, ret)=> {
+          if (err) {
+            return done(err);
+          }
+          response = ret;
+          done();
+        });
+    });
   });
 
   it('200を返す', ()=> {
@@ -32,6 +44,7 @@ describe('routes:author', ()=> {
     assert(Object.keys(response.body).length === 0);
   });
   it('意図したコンテンツを配信している', ()=> {
-    assert(response.text.match(/未森ちや先生のKindle化された著書/));
+    assert(response.text.match(/テスト先生のKindle化された著書/));
+  });
   });
 });
