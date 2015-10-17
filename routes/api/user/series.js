45d372a2773791a0d495f7d05646794c640cdf47
Shinichi Kogai <kogai0121@gmail.com>
2015-10-17 21:35:39 +0900

    [add] シリーズ削除のテスト

--- routes/api/user/series.js
+++ routes/api/user/series.js
@@ -1,57 +1,94 @@
 import moment from 'moment-timezone';
+import Promise from 'bluebird';
 import UserModel from 'models/User';
 import SeriesClass from 'Librarian/Series';
 import log from 'common/log';
 const Series = SeriesClass();
 
-module.exports = (req, res)=> {
-  const userSession = req.session.passport.user;
-  if (!userSession) {
-    return res.status(500).send();
-  }
-  const query = req.body.query;
-
-  Series.saveSeries(query, (err, newSeries)=> {
-    if (err) {
-      log.info(err);
-      return res.status(500).send(err);
+export default {
+  post(req, res) {
+    const userSession = req.session.passport.user;
+    if (!userSession) {
+      return res.status(500).send();
     }
-    const resMessage = `『${newSeries.seriesKeyword}』を登録しました。`;
-    const existMessage = `『${newSeries.seriesKeyword}』は登録済みです。`;
+    const query = req.body.query;
 
-    const conditions = {
-      _id: userSession,
-      'seriesList.seriesKeyword': {
-        $nin: [ newSeries.seriesKeyword ],
-      },
-    };
-    const update = {
-      modifiedLog: {
-        seriesListAt: moment(),
-      },
-      $push: {
-        seriesList: {
-          _id: newSeries._id,
-          seriesKeyword: newSeries.seriesKeyword,
+    Series.saveSeries(query, (err, newSeries)=> {
+      if (err) {
+        log.info(err);
+        return res.status(500).send(err);
+      }
+      const resMessage = `『${newSeries.seriesKeyword}』を登録しました。`;
+      const existMessage = `『${newSeries.seriesKeyword}』は登録済みです。`;
+
+      const conditions = {
+        _id: userSession,
+        'seriesList.seriesKeyword': {
+          $nin: [ newSeries.seriesKeyword ],
         },
-      },
-    };
-    const option = { new: true };
+      };
+      const update = {
+        modifiedLog: {
+          seriesListAt: moment(),
+        },
+        $push: {
+          seriesList: {
+            _id: newSeries._id,
+            seriesKeyword: newSeries.seriesKeyword,
+          },
+        },
+      };
+      const option = { new: true };
 
-    UserModel.findOneAndUpdate(conditions, update, option, (updateError, user)=> {
-      if (updateError) {
-        log.info(updateError);
-        return res.status(500).send(updateError);
-      }
-      if (user) {
-        res.send({
-          message: resMessage,
-        });
-      } else {
-        res.send({
-          message: existMessage,
-        });
-      }
+      UserModel.findOneAndUpdate(conditions, update, option, (updateError, user)=> {
+        if (updateError) {
+          log.info(updateError);
+          return res.status(500).send(updateError);
+        }
+        if (user) {
+          res.send({
+            message: resMessage,
+          });
+        } else {
+          res.send({
+            message: existMessage,
+          });
+        }
+      });
     });
-  });
+  },
+  delete(req, res) {
+    const userSession = req.session.passport.user;
+    if (!userSession) {
+      return res.status(500).send();
+    }
+    const seriesKeyword = req.body.query;
+    const conditions = { _id: userSession };
+    const findOne = Promise.promisify(UserModel.findOne.bind(UserModel));
+
+    findOne(conditions)
+    .then((user)=> {
+      const update = {
+        seriesList: user.seriesList.filter((series)=> {
+          if (series.seriesKeyword !== seriesKeyword) {
+            return series;
+          }
+        }),
+      };
+      return new Promise((resolve, reject)=> {
+        UserModel.findOneAndUpdate(conditions, update, (updateError)=> {
+          if (updateError) {
+            return reject(updateError);
+          }
+          resolve();
+        });
+      });
+    })
+    .then(()=> {
+      return res.status(200).send();
+    })
+    .catch((err)=> {
+      return res.status(500).send(err);
+    });
+  },
 };
--- test/routes/api/user/series.spec.js
+++ test/routes/api/user/series.spec.js
@@ -1,8 +1,8 @@
 import assert from 'power-assert';
 import mockgoose from 'mockgoose';
+import Promise from 'bluebird';
 import UserModel from 'models/User';
 import {
-  // getReq,
   loginReq,
 } from 'test/helper/request';
 import {
@@ -23,15 +23,15 @@ describe('/routes/api/user/book', function withTimeout() {
     mockgoose.reset();
   });
 
-  it.only('シリーズが保存できる', (done)=> {
-    const payload = {
-      query: `0-${defaultBook.title} (10)`,
-    };
+  it('シリーズが保存できる', (done)=> {
+    const payload = { query: `0-${defaultBook.title} (10)` };
     const conditions = { mail: `0-${defaultAccount.mail}`};
 
     loginReq()
     .then((appSession)=> {
-      appSession.post(endpoint).send(payload).end(()=> {
+      appSession.post(endpoint).send(payload).end((err, ret)=> {
+        assert(err === null);
+        assert(ret.status === 200);
         UserModel.findOne(conditions, (_, user)=> {
           assert(user.seriesList.length === 1);
           assert(user.seriesList[0].seriesKeyword === `0-${defaultBook.title}`);
@@ -41,8 +41,33 @@ describe('/routes/api/user/book', function withTimeout() {
     });
   });
 
-  /*
   it('シリーズが削除できる', (done)=> {
+    const payload = { query: `0-${defaultBook.title} (10)` };
+    const conditions = { mail: `0-${defaultAccount.mail}`};
+    let appSession;
+
+    loginReq()
+    .then((_appSession)=> {
+      appSession = _appSession;
+      return new Promise((resolve)=> {
+        appSession.post(endpoint).send(payload).end(resolve);
+      });
+    })
+    .then(()=> {
+      const deletePayload = { query: `0-${defaultBook.title}` };
+      return new Promise((resolve)=> {
+        appSession.delete(endpoint).send(deletePayload).end((err, ret)=> {
+          assert(err === null);
+          assert(ret.status === 200);
+          resolve();
+        });
+      });
+    })
+    .then(()=> {
+      UserModel.findOne(conditions, (_, user)=> {
+        assert(user.seriesList.length === 0);
+        done();
+      });
+    });
   });
-  */
 });
